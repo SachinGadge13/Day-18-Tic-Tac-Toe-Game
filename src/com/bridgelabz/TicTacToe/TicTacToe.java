8c6da828cf4ac52e684e9b5857b2a66072b13aa0
SachinGadge13 <gadges14@gmail.com>
2022-10-14 17:18:16 +0530

    Added:Program to allow the player to choose a letter X or O

--- src/com/bridgelabz/TicTacToe/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe/TicTacToe.java
@@ -1,18 +1,22 @@
 package com.bridgelabz.TicTacToe;
 
+import java.util.Scanner;
+
 public class TicTacToe {
-    static char[] board;    // declaring an array
+    static char[] board;
+    static char playerChoice;
+    static char computerChoice;
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Game Initiated.");
         createBoard();
         showBoard();
+        chooseLetter();
     }
 
     // UC1: Creating empty tic-tac-toe board
     public static void createBoard() {
-        board = new char[10];   // creating an array
-
-        // initialize an array
+        board = new char[10];
         for (int i = 1; i < board.length; i++) {
             board[i] = ' ';
         }
@@ -25,5 +29,22 @@ public class TicTacToe {
         System.out.println(board[4]+ " | " +board[5]+ "| "+board[6]);
         System.out.println("---------");
         System.out.println(board[7]+ " | " +board[8]+ "| "+board[9]);
+    }
+
+    // UC2: Getting user input to choose either X or O
+    public static void chooseLetter() {
+        System.out.println("Enter the Player Choice between X | O ");
+        playerChoice = sc.next().toUpperCase().charAt(0);
+        if(playerChoice == 'X') {
+            computerChoice = 'O';
+            System.out.println("Player Choice is : "+ playerChoice);
+            System.out.println("Computer Choice is : "+ computerChoice);
+        } else if (playerChoice == 'O'){
+            computerChoice = 'X';
+            System.out.println("Player Choice is : "+ playerChoice);
+            System.out.println("Computer Choice is : "+ computerChoice);
+        } else {
+            System.out.println("Invalid symbol.");
+        }
     }
 }
